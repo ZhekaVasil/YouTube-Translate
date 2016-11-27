563b61160024c11b3706dd11f56bc179aeac71ae
ZhekaVasil <zheka_1408@mail.ru>
2016-11-27 03:13:04 +0300

    Добавлена возможность загружать аудио на сервер

--- yt_project/src/AppBundle/Controller/SubeditorController.php
+++ yt_project/src/AppBundle/Controller/SubeditorController.php
@@ -13,10 +13,28 @@ class SubeditorController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $test = "sfdfdsfsfd";
-        // replace this example code with whatever you need
+        $id = '';
+        // проверяем есть ли POST запрос от формы
+        if($request->request->get('idvid')) {
+            //в переменную $id записываем id видюшки
+            $mas = explode('v=', $request->request->get('idvid'));
+            $part = $mas[1];
+            $id = substr($part, 0, 11);
+            // проверяем есть ли аудио в запросе
+            if(is_uploaded_file($request->files->get('audio'))) {
+                //если аудио есть, то создаем папку внутри 'preview', папку именуем как id видюшки
+                if(!is_dir('preview/'.$id)){
+                    mkdir('preview/'.$id);
+                };
+                //перемещаем аудио в свою папку, и именуем ее как 'audio.mp3'
+                move_uploaded_file($request->files->get('audio'), "preview/".$id."/audio.mp3");
+            }
+        }
+
+
+        // просто выводим id видюшки в твиге
         return $this->render('default/subeditor.html.twig',[
-            'test' => $test,
+            'id' => $id,
         ]);
     }
 }
